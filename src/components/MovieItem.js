b9df71093509c81f2bb8c71c935605c0f174a62d
dgrytsyna <dgrytsyna2004@gmail.com>
2020-03-24 22:32:35 +0200

    "React app"

---
+++ src/components/MovieItem.js
@@ -0,0 +1,64 @@
+import React from "react";
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      willWatch: false
+    };
+  }
+  render() {
+    const { movie, removeMovie, addMovieToWillWatch, removeMovieFromWillWatch } = this.props;
+    return (
+      <div className="card">
+        <img
+          className="card-img-top"
+          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path ||
+            movie.poster_path}`}
+          alt=""
+        />
+        <div className="card-body">
+          <h6 className="card-title">{movie.title}</h6>
+          <div className="d-flex justify-content-between align-items-center">
+            <p className="mb-0">Rating: {movie.vote_average}</p>
+            {this.state.willWatch ? (
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={()=>{
+                  this.setState({
+                    willWatch: false
+                  })
+                  removeMovieFromWillWatch(movie);
+                }}
+              >
+                Remove will watch
+              </button>
+            ) : (
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={() => {
+                  this.setState({
+                    willWatch: true
+                  });
+                  addMovieToWillWatch(movie);
+                }}
+              >
+                Add will watch
+              </button>
+            )}
+
+            <button
+              className="btn btn-danger"
+              onClick={removeMovie.bind(null, movie)}
+            >
+              Delete movie
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieItem;
